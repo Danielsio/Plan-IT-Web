0ca207ab836c657e6374f98519e814be5f04f669
kingd <kingddd301@gmail.com>
2023-04-11 23:18:59 +0300

    cleen white spaces in profile card

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -14,44 +14,37 @@ const ProfileCard = ({
   onClick,
 }) => {
   return (
-    
-   <>
-    
-
-    
-
-    <Row>
+    <>
+      <Row>
         <Col sm={4}>
-        <div className="Profile-Card card">
-          <div className="upper-container card-body">
-            <div className="image-container d-flex justify-content-center">
-              <img
-                src={pictureUrl}
-                alt="profile Pic"
-                height="100px"
-                width="100px"
-              />
+          <div className="Profile-Card card">
+            <div className="upper-container card-body">
+              <div className="image-container d-flex justify-content-center">
+                <img
+                  src={pictureUrl}
+                  alt="profile Pic"
+                  height="100px"
+                  width="100px"
+                />
+              </div>
+            </div>
+            <div className="lower-container card-body">
+              <h3 className="card-title">{name}</h3>
+              <h5 className="card-subtitle mb-2 text-muted">{email}</h5>
             </div>
           </div>
-      <div className="lower-container card-body">
-        <h3 className="card-title">{name}</h3>
-        <h5 className="card-subtitle mb-2 text-muted">{email}</h5>
-
-        
-
-        
-      </div>
-    </div>
         </Col>
 
         <Col sm={8}>
           <div className="Profile-Card card">
             <div className="Preferences mt-2">
               <div className="card-body">
-              <h3 className="card-title">Preferences</h3>
+                <h3 className="card-title">Preferences</h3>
                 <Row className="mb-2 row-preferences">
                   <Col xs={6}>
-                    <h6 className="mb-0 title-in-preferences">Study Start Time:</h6>
+                    <h6 className="mb-0 title-in-preferences">
+                      Study Start Time:
+                    </h6>
                   </Col>
                   <Col xs={6}>
                     <h6 className="mb-0">{userStudyStartTime}</h6>
@@ -59,7 +52,9 @@ const ProfileCard = ({
                 </Row>
                 <Row className="mb-2">
                   <Col xs={6}>
-                    <h6 className="mb-0 title-in-preferences">Study End Time:</h6>
+                    <h6 className="mb-0 title-in-preferences">
+                      Study End Time:
+                    </h6>
                   </Col>
                   <Col xs={6}>
                     <h6 className="mb-0">{userStudyEndTime}</h6>
@@ -67,7 +62,9 @@ const ProfileCard = ({
                 </Row>
                 <Row className="mb-2">
                   <Col xs={6}>
-                    <h6 className="mb-0 title-in-preferences">Break Time Size:</h6>
+                    <h6 className="mb-0 title-in-preferences">
+                      Break Time Size:
+                    </h6>
                   </Col>
                   <Col xs={6}>
                     <h6 className="mb-0">{userBreakTime} Minutes</h6>
@@ -75,7 +72,9 @@ const ProfileCard = ({
                 </Row>
                 <Row className="mb-2">
                   <Col xs={6}>
-                    <h6 className="mb-0 title-in-preferences">Study Session Size:</h6>
+                    <h6 className="mb-0 title-in-preferences">
+                      Study Session Size:
+                    </h6>
                   </Col>
                   <Col xs={6}>
                     <h6 className="mb-0">{studySessionTime} Minutes</h6>
@@ -83,7 +82,9 @@ const ProfileCard = ({
                 </Row>
                 <Row className="mb-2">
                   <Col xs={6}>
-                    <h6 className="mb-0 title-in-preferences">Study On Holidays:</h6>
+                    <h6 className="mb-0 title-in-preferences">
+                      Study On Holidays:
+                    </h6>
                   </Col>
                   <Col xs={6}>
                     <h6 className="mb-0">{studyOnHolidays ? "Yes" : "No"}</h6>
@@ -91,7 +92,9 @@ const ProfileCard = ({
                 </Row>
                 <Row className="mb-2">
                   <Col xs={6}>
-                    <h6 className="mb-0 title-in-preferences">Study On Weekends:</h6>
+                    <h6 className="mb-0 title-in-preferences">
+                      Study On Weekends:
+                    </h6>
                   </Col>
                   <Col xs={6}>
                     <h6 className="mb-0">{studyOnWeekends ? "Yes" : "No"}</h6>
@@ -106,15 +109,11 @@ const ProfileCard = ({
                   Edit Preferences
                 </Button>
               </div>
-              
-    
             </div>
           </div>
-        
         </Col>
-
       </Row>
-   </>
+    </>
   );
 };
 
