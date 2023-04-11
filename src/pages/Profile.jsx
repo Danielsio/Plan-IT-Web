6930d462f8864eb3e12540f7e8b80b8d6d8969a2
Itay Finkelshtein <itayf9@gmail.com>
2023-04-11 22:03:21 +0300

    Fix naming of Profile arguments
    Co-authored-by: oshriFadlon1 <oshriFadlon1@users.noreply.github.com>

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -17,6 +17,7 @@ import ProfileCard from "../components/ProfileCard";
 import { animated, useSpring } from "react-spring";
 
 function Profile() {
+  
   const convertUserStudyTimeToHours = (userStudyTime) => {
     const hour = Math.floor(userStudyTime / 100);
     if (hour < 10) {
@@ -64,9 +65,11 @@ function Profile() {
         console.log(error);
       }
     };
+
     if (isAuthenticated) {
       getUserData();
     }
+
   }, [isAuthenticated]);
 
   if (!isAuthenticated) {
@@ -91,8 +94,8 @@ function Profile() {
           <ProfileCard
             name={userData.profile.name}
             email={userData.profile.email}
-            picProfile={userData.profile.pictureUrl}
-            startTime={
+            pictureUrl={userData.profile.pictureUrl}
+            userStudyStartTime={
               convertUserStudyTimeToHours(
                 userData.userPreferences.userStudyStartTime
               ).toString() +
@@ -101,7 +104,7 @@ function Profile() {
                 userData.userPreferences.userStudyStartTime
               ).toString()
             }
-            endTime={
+            userStudyEndTime={
               convertUserStudyTimeToHours(
                 userData.userPreferences.userStudyEndTime
               ).toString() +
@@ -110,8 +113,8 @@ function Profile() {
                 userData.userPreferences.userStudyEndTime
               ).toString()
             }
-            breakTime={userData.userPreferences.userBreakTime}
-            SessionLength={userData.userPreferences.studySessionTime}
+            userBreakTime={userData.userPreferences.userBreakTime}
+            studySessionTime={userData.userPreferences.studySessionTime}
             studyOnHolidays={userData.userPreferences.studyOnHolidays}
             studyOnWeekends={userData.userPreferences.studyOnWeekends}
             onClick={handleEditPreferences}
