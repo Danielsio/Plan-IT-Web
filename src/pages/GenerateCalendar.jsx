ad10b4d5cac8d292e5b019d268beea2b44632734
Itay Finkelshtein <itayf9@gmail.com>
2023-04-12 15:40:52 +0300

    Fix start date and end date of Plan in generated study plan view

--- src/pages/GenerateCalendar.jsx
+++ src/pages/GenerateCalendar.jsx
@@ -208,10 +208,10 @@ const GenerateCalendar = () => {
                       </ul>
                     </div>
                     <div>
-                      Start Date Time of Plan: {studyPlan.startDateTimeOfPlan}
+                      Start Date Time of Plan: {new Date(studyPlan.startDateTimeOfPlan).toLocaleDateString()}
                     </div>
                     <div>
-                      End Date Time of Plan: {studyPlan.endDateTimeOfPlan}
+                      End Date Time of Plan: {new Date(studyPlan.endDateTimeOfPlan).toLocaleDateString()}
                     </div>
                     <div>
                       Total Number of Study Sessions:{" "}
