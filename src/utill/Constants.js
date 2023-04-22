3560e1f81677508f56e5d01ce9762356b1596b02
Itay Finkelshtein <itayf9@gmail.com>
2023-04-22 21:07:41 +0300

    Fix toast in Generate Calendar Page to look better

--- src/utill/Constants.js
+++ src/utill/Constants.js
@@ -1,7 +1,3 @@
 export const NO_PROBLEM = "No Problem Found";
 export const ERROR_FULL_DAY_EVENTS = "Unhandled Full Days Events.";
 export const ERROR_NO_EXAMS_FOUND = "No Exams Found.";
-
-export const TOAST_TONE_SUCCESS = "success";
-export const TOAST_TONE_WARNING = "warning";
-export const TOAST_TONE_ERROR = "error";
