ae291bc30bde565655fe52c18d028cef1962fc31
Itay Finkelshtein <itayf9@gmail.com>
2023-07-27 23:06:23 +0300

    Add loading placeholder to the study plan area

--- src/pages/GenerateCalendar.jsx
+++ src/pages/GenerateCalendar.jsx
@@ -3,7 +3,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import api from "../api/axiosBackendConfig";
 import { UserContext } from "../context/UserContext";
-import { Row, Button, Col, Modal, Card, Container } from "react-bootstrap";
+import { Row, Button, Col, Modal, Card, Container} from "react-bootstrap";
 import { ClipLoader } from "react-spinners";
 import "../styles/generateCalendar.css";
 import FullDayEventItem from "../components/FullDayEventItem";
@@ -20,6 +20,7 @@ import {
 } from "../utill/Constants";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import {Skeleton} from "@mui/lab";
 
 const GenerateCalendar = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -28,6 +29,7 @@ const GenerateCalendar = () => {
   const [showModal, setShowModal] = useState(false);
   const [fullDayEvents, setFullDayEvents] = useState([]);
   const [decisions, setDecisions] = useState([]);
+  const [loadingStudyDetails, setLoadingStudyDetails] = useState(true);
   const [studyPlan, setStudyPlan] = useState(null);
   const [upComingSession, setUpComingSession] = useState(null);
 
@@ -61,6 +63,7 @@ const GenerateCalendar = () => {
 
         setStudyPlan(response.data.studyPlan);
         setUpComingSession(response.data.upComingSession);
+        setLoadingStudyDetails(false);
       } catch (error) {
         console.error(error);
         setStudyPlan(null);
@@ -384,90 +387,119 @@ const GenerateCalendar = () => {
             <Card.Body>
               <Card.Title>Generated Study Plan:</Card.Title>
 
-              {studyPlan ? (
-                <>
-                  <div className="title-in-preferences">
-                    Scanned Exams:{" "}
-                    <ul>
-                      {studyPlan.scannedExams.map((exam) => {
-                        return (
-                          <li key={exam.courseName}>
-                            <ExamItem
-                              courseName={exam.courseName}
-                              dateTimeISO={exam.dateTimeISO}
-                            />
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                  <hr></hr>
-                  <Row className="mb-2 row-preferences">
-                    <Col xs={6}>
-                      <h6 className="mb-0 title-in-preferences">
-                        Start Date Time of Plan:
-                      </h6>
-                    </Col>
-                    <Col xs={6}>
-                      <h6 className="mb-0 generated-plan-value">
-                        {new Date(
-                          studyPlan.startDateTimeOfPlan
-                        ).toLocaleDateString()}
-                      </h6>
-                    </Col>
-                  </Row>
-                  <Row className="mb-2 row-preferences">
-                    <Col xs={6}>
-                      <h6 className="mb-0 title-in-preferences">
-                        End Date Time of Plan:
-                      </h6>
-                    </Col>
-                    <Col xs={6}>
-                      <h6 className="mb-0 generated-plan-value">
-                        {new Date(
-                          studyPlan.endDateTimeOfPlan
-                        ).toLocaleDateString()}
-                      </h6>
-                    </Col>
-                  </Row>
-                  <Row className="mb-2">
-                    <Col xs={6}>
-                      <h6 className="mb-0 title-in-preferences">
-                        Total Number of Study Sessions:
-                      </h6>
-                    </Col>
-                    <Col xs={6}>
-                      <h6 className="mb-0 generated-plan-value">
-                        {studyPlan.totalNumberOfStudySessions}
-                      </h6>
-                    </Col>
-                  </Row>
-                </>
+              {loadingStudyDetails ? (
+                  <>
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"50%", display: "inline-block", margin: "0 20% 0 0"}} />
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"30%", display: "inline-block"}}/>
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"50%", display: "inline-block", margin: "0 20% 0 0"}}/>
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"30%", display: "inline-block"}}/>
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"100%", display: "inline-block", margin: "0 100px 0 0"}} />
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"100%", display: "inline-block", margin: "0 100px 0 0"}} />
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"100%", display: "inline-block", margin: "0 100px 0 0"}} />
+                  </>
+
               ) : (
-                <div>No study plan found.</div>
-              )}
+                  <>
+                    {studyPlan ? (
+                        <>
+                          <div className="title-in-preferences">
+                            Scanned Exams:{" "}
+                            <ul>
+                              {studyPlan.scannedExams.map((exam) => {
+                                return (
+                                    <li key={exam.courseName}>
+                                      <ExamItem
+                                          courseName={exam.courseName}
+                                          dateTimeISO={exam.dateTimeISO}
+                                      />
+                                    </li>
+                                );
+                              })}
+                            </ul>
+                          </div>
+                          <hr></hr>
+                          <Row className="mb-2 row-preferences">
+                            <Col xs={6}>
+                              <h6 className="mb-0 title-in-preferences">
+                                Start Date Time of Plan:
+                              </h6>
+                            </Col>
+                            <Col xs={6}>
+                              <h6 className="mb-0 generated-plan-value">
+                                {new Date(
+                                    studyPlan.startDateTimeOfPlan
+                                ).toLocaleDateString()}
+                              </h6>
+                            </Col>
+                          </Row>
+                          <Row className="mb-2 row-preferences">
+                            <Col xs={6}>
+                              <h6 className="mb-0 title-in-preferences">
+                                End Date Time of Plan:
+                              </h6>
+                            </Col>
+                            <Col xs={6}>
+                              <h6 className="mb-0 generated-plan-value">
+                                {new Date(
+                                    studyPlan.endDateTimeOfPlan
+                                ).toLocaleDateString()}
+                              </h6>
+                            </Col>
+                          </Row>
+                          <Row className="mb-2">
+                            <Col xs={6}>
+                              <h6 className="mb-0 title-in-preferences">
+                                Total Number of Study Sessions:
+                              </h6>
+                            </Col>
+                            <Col xs={6}>
+                              <h6 className="mb-0 generated-plan-value">
+                                {studyPlan.totalNumberOfStudySessions}
+                              </h6>
+                            </Col>
+                          </Row>
+                        </>
+                    ) : (
+                        <div>No study plan found.</div>
+                    )}
+                  </>
+              ) }
+
+
             </Card.Body>
           </Card>
 
           <Card className="card-container generate-plan-card">
             <Card.Body>
               <Card.Title>Upcoming Study Session:</Card.Title>
-              {upComingSession ? (
-                <>
-                  <h6 className="mb-0 title-in-preferences">Course Name:</h6>
-                  <p>{upComingSession.courseName}</p>
-                  <h6 className="mb-0 title-in-preferences">Subject:</h6>
-                  <p>{upComingSession.description}</p>
-                  <h6 className="mb-0 title-in-preferences">Start Time:</h6>
-                  <p>
-                    {new Date(upComingSession.start.value).toLocaleString()}
-                  </p>
-                  <h6 className="mb-0 title-in-preferences">End Time:</h6>
-                  <p>{new Date(upComingSession.end.value).toLocaleString()}</p>
-                </>
+              {loadingStudyDetails ? (
+                  <>
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"20%", display: "inline-block", margin: "0 80% 0 0"}} />
+                    <Skeleton variant="rounded" height={20} animation="wave" style={{width:"100%", display: "inline-block", margin: "0 100px 0 0"}} />
+                  </>
+
               ) : (
-                <div>There are no sessions yet.</div>
+                  <>
+                    {upComingSession ? (
+                        <>
+                          <h6 className="mb-0 title-in-preferences">Course Name:</h6>
+                          <p>{upComingSession.courseName}</p>
+                          <h6 className="mb-0 title-in-preferences">Subject:</h6>
+                          <p>{upComingSession.description}</p>
+                          <h6 className="mb-0 title-in-preferences">Start Time:</h6>
+                          <p>
+                            {new Date(upComingSession.start.value).toLocaleString()}
+                          </p>
+                          <h6 className="mb-0 title-in-preferences">End Time:</h6>
+                          <p>{new Date(upComingSession.end.value).toLocaleString()}</p>
+                        </>
+                    ) : (
+                        <div>There are no sessions yet.</div>
+                    )}
+                  </>
               )}
+
+
             </Card.Body>
           </Card>
         </Col>
@@ -497,7 +529,7 @@ const GenerateCalendar = () => {
                 />
               </div>
               <Button variant="primary" size="lg" onClick={handleGenerate}>
-                Generate Calendar
+                Generate New Plan
               </Button>
               <Button
                 className="mt-3"
@@ -505,7 +537,7 @@ const GenerateCalendar = () => {
                 size="lg"
                 onClick={handleReGenerate}
               >
-                Re-Generate Calendar
+                Re-Generate Existing Plan
               </Button>
 
               {loading && (
