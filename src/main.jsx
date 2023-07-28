da6ef9d9c4b3221f9e839627bfaa971b4dec54b3
kingd <kingddd301@gmail.com>
2023-07-29 00:34:15 +0300

    Refactor google-client-id to be injected from .env file

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const google_client_id = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="455740199105-no6hntm3m4aknhjpidr52ms8ti2vib49.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={google_client_id}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
