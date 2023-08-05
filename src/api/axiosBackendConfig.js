0f6d1c4ac853ddc3ca86310b4d44748654a166b2
Daniel Sionov <kingddd301@gmail.com>
2023-08-05 13:25:55 +0300

    Update axiosBackendConfig.js

--- src/api/axiosBackendConfig.js
+++ src/api/axiosBackendConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = import.meta.env.DEV
     ? "http://localhost:8080"
-    : "ec2-54-167-138-30.compute-1.amazonaws.com:8080";
+    : "https://planit-itay.onrender.com";
 
 console.log(baseURL);
 export default axios.create({
