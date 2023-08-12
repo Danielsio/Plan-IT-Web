075acf9266303272f64071680022f7afa2db408b
Itay Finkelshtein <itayf9@gmail.com>
2023-08-12 14:55:02 +0300

    Fix the full days modal to look better

--- src/components/generate/FullDayEventItem.jsx
+++ src/components/generate/FullDayEventItem.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import {Col, Row} from "react-bootstrap";
+import Box from "@mui/material/Box";
 
 function FullDayEventItem({date, name, handleDecision}) {
     return (
-        <ListGroup horizontal className="m-2">
-            <ListGroup.Item>{name}</ListGroup.Item>
-            <ListGroup.Item>{new Date(date).toLocaleDateString()}</ListGroup.Item>
-            <ListGroup.Item>
-                <input type="checkbox" onChange={() => handleDecision(date)}/>
-            </ListGroup.Item>
-        </ListGroup>
+            <Row style={{margin: "15px"}}>
+                <Col style={{flexGrow: 0}}>
+                    <Box className="box-event-line"/>
+                </Col>
+                <Col>
+                    <div className="mb-0 title-in-preferences">
+                        {name}
+                    </div>
+                </Col>
+                <Col className="generated-plan-value" style={{maxWidth: "fit-content"}}>
+                    <div className="sub-text">
+                        {new Date(date).toLocaleDateString()}
+                    </div>
+                </Col>
+                <Col style={{maxWidth: "fit-content"}}>
+                    <input type="checkbox" onChange={() => handleDecision(date)}/>
+                </Col>
+            </Row>
     );
 }
 
